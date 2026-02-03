f26aad7473bd7ec833ffb8706ef70f4f2177a1ef
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-03 15:15:04 +0200

    testing the environment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <h1>
+  Hello Patrick this is my first Reacct Js Tell your neighbour that we will make it.
+ </h1>
 )
