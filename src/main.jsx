bda38b7afeeedda928101c0179590535d9223d03
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-04 14:05:35 +0200

    Adding this

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createElement } from "react"
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- <h1>
-  Hello Patrick this is my first Reacct Js Tell your neighbour that we will make it.
- </h1>
-)
+const root =createRoot(document.getElementById('root'))
+const Element=createElement("h1" ,null,"This my First React with CreateElement")
+root.render(Element)
+
