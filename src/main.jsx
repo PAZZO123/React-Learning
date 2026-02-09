f1cd70af3f7d94e8b8649ce36699a87fb5ed4655
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-09 17:42:11 +0200

    Adding to Github

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//import { createElement } from "react"
-import './index.css'
-import NavBar from './App.jsx'
-//let name='Patrick straton'
+import { createRoot } from "react-dom/client";
+import Logo from "./public/vite.svg";
 
+let root = createRoot(document.getElementById("root"));
 
+let h1 = <h1>Fun facts about React</h1>;
 
-const root =createRoot(document.getElementById('root'))
-//const Element=<h1 className='greeting'> Hello {name} How is everything</h1>
+let list = (
+  <ul>
+    <li>It was created in 2013</li>
+    <li>It is maintained by Meta</li>
+    <li>It is composable and very easy to use</li>
+  </ul>
+);
+
 root.render(
-    <NavBar/>
-)
-
+  <div>
+    <img src="{Logo}" width="40px" />
+    {h1}
+    {list}
+  </div>
+);
