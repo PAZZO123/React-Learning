17eb86dfba9b48ed7ac7d34755d37815eb639d3a
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-09 19:09:14 +0200

    Adding to Github

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,54 @@
 import { createRoot } from "react-dom/client";
 import Logo from "/src/assets/react.jpg";
+import './index.css'
 
 let root = createRoot(document.getElementById("root"));
-function Header(){
+
+function Header() {
     return (
-        <header> 
-        <img src={Logo} width="40px"/>
-          <h1>Fun Fact about React !</h1>
+        <header>
+            <img src={Logo} width="40px" alt="React logo" />
+            <nav>
+                <ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
         </header>
     )
 }
-function Footer(){
+
+function MainContent() {
     return (
-        <p>    "© 2026 patrick development. All rights reserved."</p>
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
     )
 }
 
 function Page() {
     return (
-        <ol>
-            <li>React is a popular library, so I will be able to
-            fit in with all the coolest devs out there! 😎</li>
-            <li>I am more likely to get a job as a front end developer
-            if I know React</li>
-              <li>React is a popular library, so I will be able to
-            fit in with all the coolest devs out there! 😎</li>
-            <li>I am more likely to get a job as a front end developer
-            if I know React</li>
-              <li>React is a popular library, so I will be able to
-            fit in with all the coolest devs out there! 😎</li>
-            <li>I am more likely to get a job as a front end developer
-            if I know React</li>
-        </ol>
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
     )
 }
 
 root.render(
-    <>
-    <Header/>
     <Page />
-    <Footer/>
-    </>
 )
