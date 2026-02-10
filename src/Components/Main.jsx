c9e60fd0cf1513b3cae24ad39f4db0351c7e498f
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-10 19:26:54 +0200

    Adding components

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,7 @@
 export default function  Main(){
     return (<main className="main">
         <h1>Fun Fact About React!</h1>
-        <ul>
+        <ul className="list">
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 200K stars on GitHub</li>
