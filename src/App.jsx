661c41815754738fa3ab7453ea46213921505149
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-12 15:27:26 +0200

    Adding Jokes App:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import Header from './component/Header.jsx'
-import Entry from './component/Entry.jsx'
+import Jokes from './component/Jokes.jsx'
 
 
-export default function App(){
-  return(
-    <>
-    <Header/>
-    <Entry/>
-    <Entry/>
-    </>
-  )
+export default function App() {
+    return (
+        <>
+        <Jokes 
+        setup='I got my daughter a fridge for her birthday.'
+        punchline="I can't wait to see her face light up when she opens it" />
+        <Jokes 
+        setup="He just ransomware!"
+        punchline="How did the hacker escape the police?"
+        />
+        <Jokes 
+        setup="Scurvy"
+        punchline="Why don't pirates travel on mountain roads?"
+        />
+        <Jokes 
+        setup="Swarm."
+        punchline="Why do bees stay in the hive in the winter?"
+        />
+        </>
+    )
 }
