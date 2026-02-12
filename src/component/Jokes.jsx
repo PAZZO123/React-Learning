8fddc8f7a882b8bed5b58891a5e93f5a2458dbe9
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-12 15:56:56 +0200

    Adding this to Github

--- src/component/Jokes.jsx
+++ src/component/Jokes.jsx
@@ -4,5 +4,6 @@ export default function Jokes({setup, punchline}){
         <h1>{setup} </h1>
         <h4>{punchline} </h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a laoreet elit. Suspendisse in aliquet ante. Morbi id velit eget ipsum suscipit convallis. Etiam fermentum facilisis elit ultricies ultricies. Sed sodales tortor ac diam ullamcorper, non lacinia massa euismod. Suspendisse porttitor quam mollis, ultrices ligula cursus, convallis ipsum. Mauris sed leo sit amet dolor commodo feugiat eu non nisl.</p>
+        <hr/>
         </>)
 }
