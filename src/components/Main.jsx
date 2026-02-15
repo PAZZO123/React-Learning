99fee06f6fe071ca9e59670e0fc7c680553b091a
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-15 15:13:09 +0200

    Initial Commit on Chief Claude Project

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,28 @@
 
+let Items=['Cucumber','Carrot','Cabbage']
+const listItems=Items.map(item=><li key={item}>{item}</li>)
+
+function handleSubmit(event){
+    event.preventDefault()
+    console.log('Form Submitted')
+    const formData=new FormData(event.currentTarget)
+    const forItem=formData.get('ingredient')
+    listItems.push(<li key={forItem}>{forItem}</li>)
+    console.log(forItem)
+    console.log(listItems)
+}
+
 export default function Main(){
     return (<main>
-       <form action="" className="form"> 
-        <input type="text"
+       <form onSubmit={handleSubmit} action="" className="form"> 
+        <input  type="text"
                placeholder="e.g Oregano"
-               aria-label="Add ingredient" />
+               aria-label="Add ingredient"
+               name='ingredient' />
          <button>Add Ingredient</button>
           </form>
+          <ul>
+            {listItems}
+          </ul>
     </main>)
 }
