f437c64139fc235b1290721e5d810fde48315cc6
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-15 13:53:21 +0200

    Initial Commit on Chief Claude Project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import icon from '../assets/Chef Claude Icon.png'
-
+//comment
 
 export default function Header(){
     return (<header className='header'>
         <img src={icon} alt="Claude Chief Icon" />
-        <span className='chief'>Chef Claude</span>
+        <span className='chief'>Chef Claude</span> 
          
     </header>)
 }
