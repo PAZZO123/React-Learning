c46b942a69c0e499f5af4b531317c4ffa8c17da8
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-17 15:58:15 +0200

    Adding Usestate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,20 @@
 import { useState } from "react"
 
-
-
-//let Items=
-
-
 export default function Main(){
 
 
 const [Items, setItems]=useState(['Cucumber','Carrot','Cabbage'])
 const listItems=Items.map(item=><li key={item}>{item}</li>)
 
-function handleSubmit(event){
-    event.preventDefault()
+function handleSubmit(formData){
+    //event.preventDefault()
     console.log('Form Submitted')
-    const formData=new FormData(event.currentTarget)
+    //const formData=new FormData(event.currentTarget)
     const forItem=formData.get('ingredient')
     setItems(items=>[...items, forItem])
 }
     return (<main>
-       <form onSubmit={handleSubmit} action="" className="form"> 
+       <form action={handleSubmit}  className="form"> 
         <input  type="text"
                placeholder="e.g Oregano"
                aria-label="Add ingredient"
